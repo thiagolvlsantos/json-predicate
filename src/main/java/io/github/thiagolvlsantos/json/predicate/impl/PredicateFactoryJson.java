57f276e7767bc121f9ba3abf0e2389badff6e688
thiagolvlsantos <thiago.lvl.santos@gmail.com>
2021-06-29 00:50:24 -0300

    Complexity.

--- src/main/java/io/github/thiagolvlsantos/json/predicate/impl/PredicateFactoryJson.java
+++ src/main/java/io/github/thiagolvlsantos/json/predicate/impl/PredicateFactoryJson.java
@@ -53,66 +53,89 @@ public class PredicateFactoryJson implements IPredicateFactory {
 			String key = n.getKey();
 			JsonNode value = n.getValue();
 			if (key.startsWith("$")) {
-				Class<? extends IPredicate> type = manager.get(key);
-				if (type == null) {
-					throw new JsonPredicateException("Invalid list operator: " + key + " for " + tree, null);
-				}
-				if (IPredicateArray.class.isAssignableFrom(type)) {
-					List<Predicate<Object>> list = new LinkedList<>();
-					if (value.isArray()) {
-						if (log.isDebugEnabled()) {
-							log.debug(gap + " LIST>" + type.getSimpleName() + "  " + key + ": " + value);
-						}
-						for (int i = 0; i < value.size(); i++) {
-							list.add(predicate("\t" + gap, value.get(i)));
-						}
-					} else {
-						throw new JsonPredicateException("Invalid list operator value: '" + value + "' is not a list.",
-								null);
-					}
-					try {
-						result.add(type.getConstructor(List.class).newInstance(list));
-					} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-							| InvocationTargetException | NoSuchMethodException | SecurityException e) {
-						throw new JsonPredicateException(e.getMessage(), e);
-					}
-				} else if (IPredicateWrapper.class.isAssignableFrom(type)) {
-					try {
-						result.add(type.getConstructor(Predicate.class).newInstance(predicate("\t" + gap, value)));
-					} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-							| InvocationTargetException | NoSuchMethodException | SecurityException e) {
-						throw new JsonPredicateException(e.getMessage(), e);
-					}
-				} else {
-					throw new JsonPredicateException("Invalid list operator: " + key + " is not a list.", null);
-				}
+				operations(gap, tree, result, key, value);
 			} else {
-				Iterator<Entry<String, JsonNode>> fs = value.fields();
-				while (fs.hasNext()) {
-					Entry<String, JsonNode> f = fs.next();
-					String op = f.getKey();
-					JsonNode va = f.getValue();
-					Class<? extends IPredicate> type = manager.get(op);
-					if (type == null) {
-						throw new JsonPredicateException("Invalid group operator: " + op + " for " + va, null);
-					}
-					if (IPredicateValue.class.isAssignableFrom(type)) {
-						if (log.isDebugEnabled()) {
-							log.debug(gap + " VALUE>" + type.getSimpleName() + " " + key + ": " + value);
-						}
-						try {
-							result.add(type.getConstructor(String.class, JsonNode.class, IAccess.class).newInstance(key,
-									va, access));
-						} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-								| InvocationTargetException | NoSuchMethodException | SecurityException e) {
-							throw new JsonPredicateException(e.getMessage(), e);
-						}
-					} else {
-						throw new JsonPredicateException("Invalid group operator: " + op + " is not a value.", null);
-					}
-				}
+				fields(gap, result, key, value);
 			}
 		}
 		return result.size() > 1 ? new PredicateAnd(result) : result.get(0);
+	}
+
+	private void operations(String gap, JsonNode tree, List<Predicate<Object>> result, String key, JsonNode value) {
+		Class<? extends IPredicate> type = manager.get(key);
+		if (type == null) {
+			throw new JsonPredicateException("Invalid list operator: " + key + " for " + tree, null);
+		}
+		if (IPredicateArray.class.isAssignableFrom(type)) {
+			predicateArray(gap, result, key, value, type);
+		} else if (IPredicateWrapper.class.isAssignableFrom(type)) {
+			predicateWrapper(gap, result, value, type);
+		} else {
+			throw new JsonPredicateException("Invalid list operator: " + key + " is not a list.", null);
+		}
+	}
+
+	private void predicateArray(String gap, List<Predicate<Object>> result, String key, JsonNode value,
+			Class<? extends IPredicate> type) {
+		List<Predicate<Object>> list = new LinkedList<>();
+		if (value.isArray()) {
+			if (log.isDebugEnabled()) {
+				log.debug(gap + " LIST>" + type.getSimpleName() + "  " + key + ": " + value);
+			}
+			for (int i = 0; i < value.size(); i++) {
+				list.add(predicate("\t" + gap, value.get(i)));
+			}
+		} else {
+			throw new JsonPredicateException("Invalid list operator value: '" + value + "' is not a list.",
+					null);
+		}
+		try {
+			result.add(type.getConstructor(List.class).newInstance(list));
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException | NoSuchMethodException | SecurityException e) {
+			throw new JsonPredicateException(e.getMessage(), e);
+		}
+	}
+
+	private void predicateWrapper(String gap, List<Predicate<Object>> result, JsonNode value,
+			Class<? extends IPredicate> type) {
+		try {
+			result.add(type.getConstructor(Predicate.class).newInstance(predicate("\t" + gap, value)));
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException | NoSuchMethodException | SecurityException e) {
+			throw new JsonPredicateException(e.getMessage(), e);
+		}
+	}
+
+	private void fields(String gap, List<Predicate<Object>> result, String key, JsonNode value) {
+		Iterator<Entry<String, JsonNode>> fs = value.fields();
+		while (fs.hasNext()) {
+			Entry<String, JsonNode> f = fs.next();
+			String op = f.getKey();
+			JsonNode va = f.getValue();
+			Class<? extends IPredicate> type = manager.get(op);
+			if (type == null) {
+				throw new JsonPredicateException("Invalid group operator: " + op + " for " + va, null);
+			}
+			if (IPredicateValue.class.isAssignableFrom(type)) {
+				fieldValue(gap, result, key, value, va, type);
+			} else {
+				throw new JsonPredicateException("Invalid group operator: " + op + " is not a value.", null);
+			}
+		}
+	}
+
+	private void fieldValue(String gap, List<Predicate<Object>> result, String key, JsonNode value, JsonNode va,
+			Class<? extends IPredicate> type) {
+		if (log.isDebugEnabled()) {
+			log.debug(gap + " VALUE>" + type.getSimpleName() + " " + key + ": " + value);
+		}
+		try {
+			result.add(type.getConstructor(String.class, JsonNode.class, IAccess.class).newInstance(key,
+					va, access));
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException | NoSuchMethodException | SecurityException e) {
+			throw new JsonPredicateException(e.getMessage(), e);
+		}
 	}
 }
