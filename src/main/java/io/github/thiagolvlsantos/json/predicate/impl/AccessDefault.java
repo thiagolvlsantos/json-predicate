02c7deb06a057a68a068289f66a2688e1b3b273f
thiagolvlsantos <thiago.lvl.santos@gmail.com>
2021-09-27 02:38:30 -0300

    Null path becomes 'null'

--- src/main/java/io/github/thiagolvlsantos/json/predicate/impl/AccessDefault.java
+++ src/main/java/io/github/thiagolvlsantos/json/predicate/impl/AccessDefault.java
@@ -3,6 +3,7 @@ package io.github.thiagolvlsantos.json.predicate.impl;
 import java.lang.reflect.InvocationTargetException;
 
 import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.NestedNullException;
 
 import io.github.thiagolvlsantos.json.predicate.IAccess;
 import io.github.thiagolvlsantos.json.predicate.exceptions.JsonPredicateException;
@@ -13,6 +14,8 @@ public class AccessDefault implements IAccess {
 	public Object get(Object source, String path) {
 		try {
 			return BeanUtils.getProperty(source, path);
+		} catch (NestedNullException e) {
+			return null;
 		} catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
 			throw new JsonPredicateException(e.getMessage(), e);
 		}
