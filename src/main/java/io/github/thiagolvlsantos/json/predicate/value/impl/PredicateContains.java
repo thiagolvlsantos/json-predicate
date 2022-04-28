b7f8e95e018d8f39f2e8cc1147b904ac86e7ebc1
thiagolvlsantos <thiago.lvl.santos@gmail.com>
2022-04-28 00:29:40 -0300

    Better expressions support.

--- src/main/java/io/github/thiagolvlsantos/json/predicate/value/impl/PredicateContains.java
+++ src/main/java/io/github/thiagolvlsantos/json/predicate/value/impl/PredicateContains.java
@@ -1,5 +1,9 @@
 package io.github.thiagolvlsantos.json.predicate.value.impl;
 
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Iterator;
+
 import com.fasterxml.jackson.databind.JsonNode;
 
 import io.github.thiagolvlsantos.json.predicate.IAccess;
@@ -14,6 +18,32 @@ public class PredicateContains extends AbstractPredicateValue {
 	@Override
 	public boolean test(Object t) {
 		Object tmp = unwrapp(t);
+		return check(tmp);
+	}
+
+	protected boolean check(Object tmp) {
+		if (tmp instanceof Collection<?>) {
+			for (Iterator<?> iterator = ((Collection<?>) tmp).iterator(); iterator.hasNext();) {
+				Object obj = (Object) iterator.next();
+				if (check(obj)) {
+					return true;
+				}
+			}
+			return false;
+		}
+		if (tmp != null && tmp.getClass().isArray()) {
+			for (int i = 0; i < Array.getLength(tmp); i++) {
+				Object obj = Array.get(tmp, i);
+				if (check(obj)) {
+					return true;
+				}
+			}
+			return false;
+		}
+		return innerCheck(tmp);
+	}
+
+	protected boolean innerCheck(Object tmp) {
 		return String.valueOf(tmp).contains(value.asText());
 	}
 }
--- src/test/java/io/github/thiagolvlsantos/json/predicate/JsonPredicateTest.java
+++ src/test/java/io/github/thiagolvlsantos/json/predicate/JsonPredicateTest.java
@@ -2,6 +2,7 @@ package io.github.thiagolvlsantos.json.predicate;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Predicate;
@@ -31,6 +32,12 @@ public class JsonPredicateTest {
 		map.put("b", true);
 		map.put("c", "any");
 		map.put("d", new String("Testing byte array!").getBytes());
+		map.put("e", Arrays.asList("a", "b", "c"));
+		map.put("f", Arrays.asList(1, 2, 3));
+		map.put("g", new String[] { "a", "b", "c" });
+		map.put("h", new int[] { 1, 2, 3 });
+		map.put("i", new int[][] { new int[] { 1, 2 }, new int[] { 3, 4 } });
+		map.put("j", new String[][] { new String[] { "alfa", "beta" }, new String[] { "gama", "teta" } });
 		System.out.println("MAP:" + map);
 	}
 
@@ -52,7 +59,17 @@ public class JsonPredicateTest {
 				"{\"c\":{\"$nmatch\":\"her\"}}", //
 				"{\"$and\": [ {\"b\": {\"$eq\": true} }, {\"a\":{\"$eq\":1}} ] }", //
 				"{\"$or\": [ {\"a\":{\"$eq\":1}}, {\"b\":{\"$eq\": false} } ] }", //
-				"{\"d\":{\"$contains\":\"array\"}}" //
+				"{\"d\":{\"$contains\":\"array\"}}", //
+				"{\"e\":{\"$contains\":\"a\"}}", //
+				"{\"f\":{\"$contains\":\"1\"}}", //
+				"{\"g\":{\"$contains\":\"b\"}}", //
+				"{\"h\":{\"$contains\":\"2\"}}", //
+				"{\"i\":{\"$contains\":\"4\"}}", //
+				"{\"j\":{\"$contains\":\"am\"}}", //
+				"{\"j.[0]\":{\"$contains\":\"beta\"}}", //
+				"{\"j.[0]\":{\"$ncontains\":\"gama\"}}", //
+				"{\"j.[0].[0]\":{\"$eq\":\"alfa\"}}", //
+				"{\"j.[0].[1]\":{\"$ne\":\"alfa\"}}", //
 		};
 	}
 
